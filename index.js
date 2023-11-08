2c281187fcfaf199e3373cd36e6c1aaed69a1593
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-11-07 23:38:38 -0600

    finished implementing read and create

--- index.js
+++ index.js
@@ -3,6 +3,55 @@ const app = express();
 
 app.use(express.static(__dirname + '/client'))
 
+
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended: true}))
+
+const mongoose = require("mongoose")
+
+const mongooseUri = "mongodb+srv://JoseMDO:mongodbpassword123@cluster0.wupcpm1.mongodb.net/movie_database"
+mongoose.connect(mongooseUri)
+const movieSchema = {
+	title: String,
+	comments: String
+}
+
+const Movie = mongoose.model("movie", movieSchema)
+
+app.post("/create", function (req, res) {
+	let newNote = new Movie({
+		title: req.body.title,
+		comments: req.body.comments
+	})
+	newNote.save();
+	res.redirect("/")
+})
+
+const renderNotes = (notesArray) => {
+	let text = "Movies Collection:\n\n"
+	notesArray.forEach((note) => {
+		text += "Title: " + note.title + "\n"
+		text += "Comments: " + note.comments + "\n"
+		text += "ID: " + note._id + "\n\n"
+	});
+	text += "Total Count: " + notesArray.length 
+	return text
+}
+
+app.get("/read", function(req, res){
+	Movie.find({}).then(notes => {
+		res.type("text/plain")
+		res.send(renderNotes(notes))
+	})
+})
+
+
+
+
+
+
+
+
 const port = process.env.PORT || 3000
 app.get('/test', function(request, response) {
 	response.type('text/plain')
