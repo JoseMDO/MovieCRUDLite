73b638977c798a2740f96fb92f3115dac4f1c6e2
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-11-27 22:07:21 -0600

    added app.use isauth

--- index.js
+++ index.js
@@ -81,14 +81,16 @@ const isAuth = (req, res, next) => {
 	}
   };
 
-app.get("/", isAuth, (req, res) => {
+app.use(isAuth)
+
+app.get("/", (req, res) => {
 	console.log("logged in 2: ", req.user)
 	res.sendFile(__dirname + "/client/main.html")
 })
 const protectedRoutes = ["/create.html", "/dashboard.html", "/delete.html", "/main.html", "/update.html"];
 
 protectedRoutes.forEach(route => {
-  app.get(route, isAuth, (req, res) => {
+  app.get(route, (req, res) => {
     console.log(`logged in 2: ${req.user} accessing ${route}`);
     res.sendFile(__dirname + `/client${route}`);
   });
